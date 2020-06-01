9492b385421799d3a90d282729719fd7655d96c2
William Pyle <wt.pyle@gmail.com>
2020-06-01 18:06:51 -0400

    Initial Commit
    Adding class to help with exposing editor buttons for methods and a class to help get all ScriptableObjects of a type from the project folders.

---
+++ ScriptableObject_Utility.cs
@@ -0,0 +1,31 @@
+﻿//Created by William Pyle 2020 http://wpyle.com
+
+using System.Collections.Generic;
+using UnityEditor;
+
+public static class ScriptableObject_Utility
+{
+    /// <summary>
+    /// Finds all scriptable objects of type in project folder and returns them as a List of ScriptableObjects
+    /// </summary>
+    /// <typeparam name="T">Type to search for</typeparam>
+    /// <typeparam name="folderPath">A specific folder to look in</typeparam>
+    /// <returns>List of scriptable objects as passed in type T</returns>
+    public static List<T> GetAllAssetsOfType<T>(string folderPath = "Assets") where T : UnityEngine.ScriptableObject
+    {
+        List<T> scriptableObjects = new List<T>();
+
+        string[] p = { folderPath };
+        string[] guids = AssetDatabase.FindAssets(string.Format("t:{0}", typeof(T)), p);
+
+        foreach (string guid in guids)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(guid);
+            T so = AssetDatabase.LoadAssetAtPath(path, typeof(T)) as T;
+
+            if (so != null) scriptableObjects.Add(so);
+        }
+
+        return scriptableObjects;
+    }
+}
